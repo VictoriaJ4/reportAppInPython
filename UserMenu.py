56000c0774cec1e26484c9b0db5015929d7b46c8
Victoria Juszkiewicz (Tallaght TC Student) <v.juszkiewicz24@ddletbtc.ie>
2025-02-07 12:21:09 +0000

    top level menu

---
+++ UserMenu.py
@@ -0,0 +1,25 @@
+
+
+def top_level_menu():
+ print(" ================ Select option =============")
+ print(" 1) Modules")
+ print(" 2) Students")
+ print(" 3) Marks")
+ print(" 4) Grades")
+ print(" or press 0 to exit")
+ print("============================================")
+
+def user_input():
+ user_input= int(input("Select > "))
+ print (user_input)
+ return user_input
+
+
+def conditional_dispaly():
+ pass
+
+def main():
+ top_level_menu()
+ user_input()
+
+main()

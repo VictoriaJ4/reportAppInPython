db66a28fb13bd4557b5018d8e09a1162a5382c62
Victoria Juszkiewicz (Tallaght TC Student) <v.juszkiewicz24@ddletbtc.ie>
2025-02-10 11:35:49 +0000

    students addign fixed

--- Students.py
+++ Students.py
@@ -9,42 +9,46 @@ def read_student_file():
     print (data_lines)
     print("==================================")
  except FileNotFoundError:
+    print("File does not exsist, creating new file...")
     create_student_file()
-    print("Error reading file.File does not exsist or is used by another user.")
     return 0
  
 
 def create_student_file():
    global student_file_path
    file_text= open(student_file_path, "w")
-   ##add header as first row
-   header_line=file_text.write("Name", "LastName", "Age"+"\n")
+   #write() only accepts a single string argument
+   header_line=file_text.write("Name,LastName,Age\n") 
    ##leave space for 8 learners?
    file_text.close()
    print("File created successfully")
    
  
-def add_student():
-    while(True):
+def get_student_details():
+    while True:
      user_answer_add_student=input("Do you want to add student? [y/n]")
      if user_answer_add_student== "y" or user_answer_add_student=="Y":
         student_name=input("Add student first name >")
         student_last_name=input("Add student last name >")
         student_age=input("Add student age >")
-        return(student_name,student_last_name, student_age)
+        insert_student_details(student_name,student_last_name, student_age)
      else:
         exit()    
 
 
-def save_student_details():
-    student_info=add_student()##saves student info into variable
-    print(student_info)
+def insert_student_details(name,lastname,age):
+    student_info=[name,lastname,age]
+    file_text= open(student_file_path, "a")
+    file_text.write(",".join(student_info) + "\n")
+    # add_user_imput=file_text.write(f"{student_info}\n") 
+    print("Student details added successfully.")
+    print("===============================")
     
-
-
+##we need only 8 students so we need a checker of how many students there are and if <8 add if more than 8 stop
 def main():
     read_student_file()
-    #add_student()
+    get_student_details()
+    insert_student_details()
     
     #save_student_details()
 main()    

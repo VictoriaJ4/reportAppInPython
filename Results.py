3496c480707d3dd351aed09102a939bbb0d8f499
Victoria Juszkiewicz (Tallaght TC Student) <v.juszkiewicz24@ddletbtc.ie>
2025-02-12 14:57:50 +0000

    results fixed and generates report

--- Results.py
+++ Results.py
@@ -1,76 +1,77 @@
-import csv
 results_file_path="C:\\Users\\VictoriaJuszkiewicz(\\Desktop\\results.csv"
 modules_file_path= "C:\\Users\\VictoriaJuszkiewicz(\\Desktop\\Modules.csv"
 students_file_path= "C:\\Users\\VictoriaJuszkiewicz(\\Desktop\\students.csv"
 
-#initially empty but we read filelines into arrays
+#initially empty but we read filelines into these arrays
 modules=[]
 students=[]
 grades=[]
 results=[]
 
-
-#read modules from file
+#read modules from modules csv file and pushes info into array modules
 def get_modules_from_file():
    global modules_file_path
    modules_file=open(modules_file_path, "r")
-   modules_lines=modules_file.readlines()
+   modules_lines=modules_file.readlines()##reads into array of strings
 
-   for module in modules_lines[1:]:
-      stripped_module = module.strip()  
+   for module in modules_lines[1:]:##loop through all modules and skip the header
+      stripped_module = module.strip()  ## remove whitespaces
       modules.append(stripped_module)
       
-   return modules
+   modules_file.close()
+   return modules ##returns only modules 4
+
+
 #read students from file
 def read_students_file():
- #global students_file_path
+ global students ##without it it won't push students into array of students and array will stay empty
  students_file=open(students_file_path, "r")
  student_lines=students_file.readlines()[1:]
- students = [line.strip().split(",") for line in student_lines]
-   
+ stripped_students = [line.strip().split(",") for line in student_lines]
+ students.extend(stripped_students) ##cant use append  here it will create 3d array 
  print(students)
  students_file.close()
  return students  
 
-#define results header
+
+#define results header, gets modules that will be part of our header and adds few fields before it separated by commas
 def results_header(modules):
  for module in modules:
   results_header = f"Firstname,Lastname,Age,{','.join(modules)}"
- print("header",results_header)
  return results_header
-
 
 
 #enter student results
 def enter_student_results():
    # enter results for each student and each module
    global results
-   students=read_students_file()
+   students=read_students_file()##reads students and adds them to students variable that is a list [[nina,dobrev,24]] 
 
    for student in students:
       #students.append(student)
-      print(f" === Results for : {student[0]},{student[1]} ===")
-      result = ",".join(student)
+      print(f" === Results for : {student[0]} {student[1]} ===")# shows current student name and last name
+      result = ",".join(student) #Converts student details into a comma-separated string (e.g., "nina,dobrev,64")
+
       for module in modules:
         marks=input(f"Please enter results for {module} >")
         result +=","+ str(marks)
         
       results.append(result)
-   print(results)
    return results
 
-def write_results_file(results,header,results_file_path):
+#gets results from enter_students_results() and writing all into a file
+def write_results_file(results,results_file_path):
+   header=results_header(modules) ## Firstname,Lastname,Age,module1,module2,module3,module4
    try:
     result_file=open(results_file_path, "w")
-    writer = csv.writer(result_file)
-    writer.writerow(header.split(","))
+    result_file.write(header + "\n")
 
     for result in results:
-        writer.writerow(result.split(","))
-        print(result.split(","))
+        result_file.write(result + "\n")
 
     result_file.close()
     print(f"Results have been written to {results_file_path}")
+    exit()
    except FileNotFoundError:
      print("No file found. Create a file first.")
      exit()
@@ -80,34 +81,10 @@ def write_results_file(results,header,results_file_path):
 
 
 def main():
- get_modules_from_file()
- #read_students_file()
- header=results_header(modules)
+ get_modules_from_file() ##works fine
+ #header=results_header(modules)  DON'T need to call it here but rather inside write function as we need header only for csv file
  enter_student_results()
- write_results_file(results,header,results_file_path)
+ write_results_file(results,results_file_path)
 
 
 main()
-
-##bin
-   
-   # global results_file_path
-   # results_file=open(results_file_path, "w")
-   # all_modules=get_modules()##this is an arrray
-   
-   # header_line = ",".join(all_modules) + "\n"        
-   # lines= []
-   # lines = add_students_to_grades_file()
-   
-   # results_file.write(f"FirstName,LastName,Age,{header_line}") 
-   # results_file.writelines (lines)
-   # results_file.close()
-   # print("File created successfully ✅") 
-
-   # def add_students_to_grades_file():
-#    students=read_students_from_file()   
-#    lines = []
-#    for student in students:
-#       lines.append( f"{student[0]},{student[1]},{student[2]} \n")
-
-#    return lines

11e6571e67fb447890dcb3c250208076ed7d4d59
Victoria Juszkiewicz (Tallaght TC Student) <v.juszkiewicz24@ddletbtc.ie>
2025-02-13 12:01:07 +0000

    some updates

--- Grades.py
+++ Grades.py
@@ -13,21 +13,30 @@
 results_path="C:\\Users\\VictoriaJuszkiewicz(\\Desktop\\results.csv"
 
 student_data=[]
+grades_array=[]
+final_cert_header=""
 
 #reads results csv file into lines skipping header
 def read_results_file(results_path):
+    global student_data
     results_file_data= open(results_path, "r")
     data_lines = results_file_data.readlines()[1:]
-    #print("From first function",data_lines)
-    return data_lines ##these are all students and their data no header
+    #student_data=data_lines
+    student_data=data_lines
+    return student_data ##these are all students and their data no header
 
+def create_header_for_cert():
+    global final_cert_header
+    results_file_data= open(results_path, "r")
+    final_cert_header=results_file_data.readline().strip("\n")
+    return final_cert_header
 
 #Extract student data function
 def extract_student_data():
-    data_lines=read_results_file(results_path)
+    global student_data
     students=[]
     # Step 2: Loop through each student record
-    for student in data_lines:
+    for student in student_data:
         splitted_data = student.strip().split(",")
         students.append(splitted_data)
 
@@ -40,7 +49,7 @@ def extract_student_data():
 def get_marks():
     all_students=extract_student_data()
     #print("third function",all_students)
-    grades_array=[]
+    global grades_array
 
     for i in range(len(all_students)):##runs as many times as we have students
         #print(f"Processing student {i}: {all_students[i]}")
@@ -54,16 +63,16 @@ def get_marks():
             grades_array.append(int(grades))##save all grades in an array grades
                     
     
-    print(grades_array)
+    #print(grades_array)
     return grades_array ##returns array
 
 
 #Create a function calculate_certification(grades_list) to determine the final certification.
 def calculate_final_cert():
-    all_grades=get_marks() ##array of grades
+    global grades_array
     grade_labels=[] ##initially empty
 
-    for i in all_grades:
+    for i in grades_array:
         if i== 50:
             grade_labels.append("Unsucesfull")
             print("Unsucesfull")
@@ -82,15 +91,29 @@ def calculate_final_cert():
 
 ##create new file with all grades and calculations
 def create_final_cert():
- pass
+ header=final_cert_header
+ print("hello from create final cert")
+
 
 ##user menu for creating certs
 def user_menu_for_final_cert():
-    input(" Do you want to generate final cert? [y/n]")
+    user_answer=input(" Do you want to generate final cert? [y/n]").upper()
+    if user_answer== "Y":
+        create_final_cert()
+        
+        exit()
+    elif user_answer== "N":
+        exit()
+    else:
+        print("Invalid answer")
 
 def main():
-    #extract_student_data()
-    #calculate_grades()
+    read_results_file(results_path) ##we need to read allresults file first 
+    create_header_for_cert()
+    #print(student_data)
+    get_marks()
+    #print(grades_array)
     calculate_final_cert()
+    user_menu_for_final_cert()
 
 main()
